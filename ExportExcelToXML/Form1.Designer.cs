757ab22c579016d1c428e1d91c667a3f1b5b0f01
wanglifeng <wang.lifeng@careerbuilder.com>
2013-08-13 10:08:56 +0800

    init project

---
+++ ExportExcelToXML/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace ExportExcelToXML
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnChooseFile = new System.Windows.Forms.Button();
+            this.grpBoxFields = new System.Windows.Forms.GroupBox();
+            this.flwPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cmbxSheets = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.grpBoxFields.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AutoSize = true;
+            this.groupBox1.Controls.Add(this.btnChooseFile);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(360, 66);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Excel Path";
+            // 
+            // btnChooseFile
+            // 
+            this.btnChooseFile.Location = new System.Drawing.Point(12, 24);
+            this.btnChooseFile.Name = "btnChooseFile";
+            this.btnChooseFile.Size = new System.Drawing.Size(75, 23);
+            this.btnChooseFile.TabIndex = 0;
+            this.btnChooseFile.Text = "Choose Excel";
+            this.btnChooseFile.UseVisualStyleBackColor = true;
+            this.btnChooseFile.Click += new System.EventHandler(this.btnChooseFile_Click);
+            // 
+            // grpBoxFields
+            // 
+            this.grpBoxFields.Controls.Add(this.flwPanel);
+            this.grpBoxFields.Location = new System.Drawing.Point(13, 148);
+            this.grpBoxFields.Name = "grpBoxFields";
+            this.grpBoxFields.Size = new System.Drawing.Size(360, 169);
+            this.grpBoxFields.TabIndex = 1;
+            this.grpBoxFields.TabStop = false;
+            this.grpBoxFields.Text = "Fields";
+            // 
+            // flwPanel
+            // 
+            this.flwPanel.Location = new System.Drawing.Point(7, 20);
+            this.flwPanel.Name = "flwPanel";
+            this.flwPanel.Size = new System.Drawing.Size(347, 143);
+            this.flwPanel.TabIndex = 0;
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(298, 346);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.cmbxSheets);
+            this.groupBox2.Location = new System.Drawing.Point(13, 86);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(354, 46);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "WorkSheets";
+            // 
+            // cmbxSheets
+            // 
+            this.cmbxSheets.FormattingEnabled = true;
+            this.cmbxSheets.Location = new System.Drawing.Point(12, 20);
+            this.cmbxSheets.Name = "cmbxSheets";
+            this.cmbxSheets.Size = new System.Drawing.Size(121, 21);
+            this.cmbxSheets.TabIndex = 0;
+            this.cmbxSheets.SelectedValueChanged += new System.EventHandler(this.cmbxSheets_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 381);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.grpBoxFields);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Export Excel To XML";
+            this.groupBox1.ResumeLayout(false);
+            this.grpBoxFields.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnChooseFile;
+        private System.Windows.Forms.GroupBox grpBoxFields;
+        private System.Windows.Forms.FlowLayoutPanel flwPanel;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox cmbxSheets;
+    }
+}
+
---
+++ ExportExcelToXML/Form1.cs
@@ -0,0 +1,118 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace ExportExcelToXML
+{
+    public partial class Form1 : Form
+    {
+        private String excelPath = string.Empty;
+        private Dictionary<int, String> mapping = new Dictionary<int, String>();
+        private ExcelWorksheet sheet = null;
+        private ExcelPackage package = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChooseFile_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dg = new OpenFileDialog() { DefaultExt = "xls,xlxs", CheckFileExists = true })
+            {
+                if (dg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    if (!String.IsNullOrEmpty(dg.FileName))
+                    {
+                        excelPath = dg.FileName;
+                        package = new ExcelPackage(new FileInfo(excelPath));
+                        //cmbxSheets.DataSource = package.Workbook.Worksheets;
+                        List<String> lists = new List<string>();
+                        lists.AddRange(package.Workbook.Worksheets.Select(t => t.Name).ToList());
+                        //sheet = package.Workbook.Worksheets["Sheet1"];
+                        //InitFieldsMapping(sheet);
+                        cmbxSheets.DataSource = lists;
+                    }
+                }
+            }
+        }
+
+        private void InitFieldsMapping(ExcelWorksheet sheet)
+        {
+            mapping.Clear();
+            flwPanel.Controls.Clear();
+            //create a row
+            if (sheet.Dimension.End.Row > sheet.Dimension.Start.Row)
+            {
+                for (int i = sheet.Dimension.Start.Column; i <= sheet.Dimension.End.Column; i++)
+                {
+                    var range = sheet.Cells[1, i];
+                    this.flwPanel.Controls.Add(new Label() { Text = range.Text, Width = flwPanel.Width / 2 - 10 });
+                    this.flwPanel.Controls.Add(new TextBox() { Width = flwPanel.Width / 2 - 10 });
+                    mapping.Add(i, range.Text);
+                }
+            }
+
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            Dictionary<String, String> nameMapping = new Dictionary<string, string>();
+            for (int i = 0; i < flwPanel.Controls.Count; i = i + 2)
+            {
+                nameMapping.Add(flwPanel.Controls[i].Text.Replace(" ", "_"), (flwPanel.Controls[i + 1] as TextBox).Text);
+            }
+            var keys = mapping.Keys.ToList();
+            foreach (int key in keys)
+            {
+                var value = mapping[key].Replace(" ", "_");
+                mapping[key] = nameMapping[value];
+            }
+
+            SaveFileDialog dialog = new SaveFileDialog() { DefaultExt = "xml", Filter = "XML file(*.xml)|*.xml", OverwritePrompt = true, AddExtension = true };
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                XmlWriter writer = new XmlTextWriter(dialog.FileName, Encoding.UTF8);
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Records");
+                for (int rowIndex = sheet.Dimension.Start.Row + 1; rowIndex <= sheet.Dimension.End.Row; rowIndex++)
+                {
+                    writer.WriteStartElement("Record");
+
+                    foreach (KeyValuePair<int, string> pair in mapping)
+                    {
+                        writer.WriteStartElement(pair.Value);
+                        writer.WriteCData(sheet.Cells[rowIndex, pair.Key].Text);
+                        writer.WriteEndElement();
+
+                        if (pair.Value == "FirstName")
+                        {
+                            writer.WriteElementString("LastName", string.Empty);
+                        }
+                    }
+
+                    writer.WriteEndElement();
+                }
+                writer.WriteEndElement();
+                writer.Flush();
+                writer.Close();
+                MessageBox.Show("Success");
+            }
+
+        }
+
+        private void cmbxSheets_SelectedValueChanged(object sender, EventArgs e)
+        {
+            sheet = package.Workbook.Worksheets[cmbxSheets.SelectedValue.ToString()];
+            InitFieldsMapping(sheet);
+        }
+    }
+}
